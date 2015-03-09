df40ad9da484e6150c58797dd26a57ab0976d2a6
Samuel Loury <samuel.loury@cosmo-platform.org>
2015-03-09 11:26:37 +0100

    Make sure the program stops if the bibtex import fails

--- lib/xapers/sources/exec.py
+++ lib/xapers/sources/exec.py
@@ -9,4 +9,5 @@ description = "Launch external program to get the bibtex"
 def fetch_bibtex(id):
     p = subprocess.Popen(shlex.split(id), stdout=subprocess.PIPE)
     p.wait()
+    assert p.returncode == 0, "The command {} failed".format(id)
     return p.stdout.read().decode("utf-8")
